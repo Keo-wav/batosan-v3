c8f03f0ca0d554debe559a2743c5c98d3c732ff7
Nicolas NACKPHOUMINH <n.nackphouminh@gmail.com>
2024-09-27 12:37:45 +0200

    added routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { Exercise1Component } from "./components/exercise1/exercise1.component";
+import { WordsComponent } from "./components/words/words.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/exercise1', pathMatch: 'full' },
+  { path: 'exercise1', component: Exercise1Component },
+  { path: 'words', component: WordsComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
